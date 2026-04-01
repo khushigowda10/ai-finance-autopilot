5ceb58ca91ea9db215432f96614cad5bd9cb6c6e
Khushi <khushigowda7810@gmail.com>
2026-04-01 18:00:55 +0530

    fix db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "ai-finance-backend-0chy.onrender.com",
-  database: "ai_finance_autopilot",
-  password: "root123",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
