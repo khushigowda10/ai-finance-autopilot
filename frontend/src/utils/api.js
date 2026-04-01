09c87389e67d92fa12af4e633fed79bce554c1f2
Khushi <khushigowda7810@gmail.com>
2026-04-01 17:07:42 +0530

    fix api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ai-finance-backend-0chy.onrender.com",  
 });
 
 // attach token automatically
