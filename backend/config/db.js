eec1e3f091635ddcf104d44e10a92dd31ee89d47
Khushi <khushigowda7810@gmail.com>
2026-04-01 17:37:57 +0530

    fix error

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host: "localhost",
+  host: "ai-finance-backend-0chy.onrender.com",
   database: "ai_finance_autopilot",
   password: "root123",
   port: 5432,
